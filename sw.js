0335fd6b2df43fb52fb78e60efe00c7f6b0eecaf
rustbasic <127506429+rustbasic@users.noreply.github.com>
2026-02-05 10:42:23 +0900

    ezClip v0.4.2
    ezClip v0.4.2

---
+++ sw.js
@@ -0,0 +1,40 @@
+var cacheName = 'ezclip-pwa-v040';
+var filesToCache = [
+  './',
+  './index.html',
+  './ezclip.js',
+  './ezclip_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    }).then(() => {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
